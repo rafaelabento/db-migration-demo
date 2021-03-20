dc8c22b3532698b4069de0933c80ebfca3249d89
Rafaela Bento <rafaelasbento@yahoo.com.br>
2021-03-19 23:01:35 -0300

    exercicios

---
+++ db-migration-demo/migrations/20210320011559-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.up = function(db, callback) {
+  db.createTable('user', {
+    id: {
+      type: 'int',
+      primaryKey: true
+    },
+    full_name: {
+      type: 'string',
+      length: 40
+    },
+    dob: {
+      type: 'date'
+    },
+    email: {
+      type: 'string',
+      length: 50
+    },
+  }, function(err) {
+    if (err) return callback(err);
+    return callback();
+  });
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('user', callback);
+};
